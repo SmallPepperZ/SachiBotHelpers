49595957e302cffc39d5db19a14e70a81a4a5ece
SmallPepperZ <69822582+SmallPepperZ@users.noreply.github.com>
2021-12-11 13:04:37 -0800

    Only config import if config is loaded

--- __init__.py
+++ __init__.py
@@ -1,1 +1,5 @@
-from .database import config
+import os
+with open(os.path.join(os.getcwd(),"storage/helpers.cfg")) as f:
+    modules = f.read().split('\n')
+if "config" in modules:
+    from .database.config_obj import config
