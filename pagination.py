87df33bc3ce4d14b2bbdfa45a318c34c193884f8
SmallPepperZ <69822582+SmallPepperZ@users.noreply.github.com>
2021-12-11 16:37:24 -0800

    Add pagination

---
+++ pagination.py
@@ -0,0 +1,63 @@
+import discord
+from discord.embeds import Embed
+
+class _PageCount(discord.ui.Button):
+    def __init__(self, paginator):
+        self.paginator=paginator
+        self.index = paginator.index
+        self.count = len(paginator.embeds)
+        super().__init__(
+            label=self.label,
+            style=discord.ButtonStyle.gray,
+            disabled=True
+        )
+
+    @property
+    def label(self) -> str:
+        return f'Page {self.index+1}/{self.count}'
+
+class EmbedPaginator(discord.ui.View):
+    def __init__(self, embeds:list[discord.Embed]):
+        super().__init__()
+        self.index = 0
+        self.embeds=embeds
+        self.count = len(embeds)
+
+    @property
+    def current_embed(self):
+        return self.embeds[self.index]
+    
+    @property
+    def label(self) -> str:
+        return f'Page {self.index+1}/{self.count}'
+
+    async def update_message(self, interaction:discord.Interaction):
+        await interaction.response.edit_message(embed=self.current_embed)
+        await self.children[2].callback(interaction)
+        await self.refresh()
+
+    @discord.ui.button(label="<<", style=discord.ButtonStyle.blurple)
+    async def first(self, button:discord.ui.Button, interaction:discord.Interaction):
+        self.index = 0
+        await self.update_message(interaction)
+
+    @discord.ui.button(label="<", style=discord.ButtonStyle.blurple)
+    async def previous(self, button:discord.ui.Button, interaction:discord.Interaction):
+        self.index -= 1 if self.index > 0 else 0
+        await self.update_message(interaction)
+
+    # @discord.ui.button(label="Page 1", style=discord.ButtonStyle.gray, disabled=True)
+    # async def label_button(self, button:discord.ui.Button, interaction:discord.Interaction):
+    #     button.label = self.label
+    #     print(button.label)
+
+
+    @discord.ui.button(label=">", style=discord.ButtonStyle.blurple)
+    async def next(self, button:discord.ui.Button, interaction:discord.Interaction):
+        self.index += 1 if self.index < len(self.embeds)-1 else 0
+        await self.update_message(interaction)
+
+    @discord.ui.button(label=">>", style=discord.ButtonStyle.blurple)
+    async def last(self, button:discord.ui.Button, interaction:discord.Interaction):
+        self.index = 5
+        await self.update_message(interaction)
