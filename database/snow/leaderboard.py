d00fa6640be63e5b6a604daa9c1b7930da10af29
SmallPepperZ <69822582+SmallPepperZ@users.noreply.github.com>
2021-12-11 19:11:16 -0800

    Only rank users who've thrown snowballs

--- database/snow/leaderboard.py
+++ database/snow/leaderboard.py
@@ -3,12 +3,13 @@ from .db_entries import SnowFighter
 class Leaderboard():
     @db_session
     def __init__(self):
-        users = list(select(user for user in SnowFighter))
+        users:list[SnowFighter] = list(select(user for user in SnowFighter))
         users.sort(key=lambda x:x.score, reverse=True)
-        self.users = users
+        self.users = [user for user in users if user.snowballs_thrown > 0]
 
     @db_session
     def get_rank(self, user_id:int):
         for index, user in enumerate(self.users):
             if int(user.user_id) == user_id:
                 return index+1
+        return 0
