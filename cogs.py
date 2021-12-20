81bab49a2bdd62780844c963021ca6f73de4ec70
SmallPepperZ <69822582+SmallPepperZ@users.noreply.github.com>
2021-12-20 13:16:00 -0800

    feat: update cog name formatting methods

--- cogs.py
+++ cogs.py
@@ -1,2 +1,18 @@
-def format_cog_name(cog_name:str) -> str:
+def pretty_cog_name(cog_name:str) -> str:
 	return cog_name.replace('cogs.', '').replace('_', ' ').title().replace('.','/')
+
+
+
+
+def pretty_cog_list(cogs:list[str]) -> list[str]:
+	formatted_names = []
+	for cog in cogs:
+		formatted_names.append(pretty_cog_name(cog))
+	return formatted_names
+
+
+def format_as_path(cog_name:str) -> str:
+	return f'cogs.{cog_name}'.replace(' ', '_').lower().replace('/','.')
+
+
+
